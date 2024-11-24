403dd0c4f6fe19f5cc3002d98172960df78661cd
AnthonyG21 <126922230+AnthonyG21@users.noreply.github.com>
2024-11-23 21:00:06 -0400

    fix
    fix

--- prisma/seed.js
+++ prisma/seed.js
@@ -8,18 +8,18 @@ async function main() {
   await prisma.users.deleteMany();
   console.log("Tabla 'users' limpiada.");
 
-  for (let u of users) {
-    const hashedPassword = await bcrypt.hash(u.password, 10);
+  for (let user of users) {
+    const hashedPassword = await bcrypt.hash(user.password, 10);
 
     // Crea el usuario con la contraseña hasheada en la base de datos
     await prisma.users.create({
       data: {
-        name: u.name,
-        email: u.email,
+        name: user.name,
+        email: user.email,
         password: hashedPassword, // Guarda la contraseña hasheada
       },
     });
-    console.log(`Usuario ${u.name} registrado.`);
+    console.log(`Usuario ${user.name} registrado.`);
   }
 }
 
