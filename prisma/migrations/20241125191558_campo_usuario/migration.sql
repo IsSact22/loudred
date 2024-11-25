dc70420db9a23edfedbf26799ed5564edfed385e
AnthonyG21 <126922230+AnthonyG21@users.noreply.github.com>
2024-11-25 19:22:39 -0400

    ajustes usuario

--- prisma/migrations/20241125191558_campo_usuario/migration.sql
+++ prisma/migrations/20241125191558_campo_usuario/migration.sql
@@ -1,5 +1,5 @@
--- AlterTable
-ALTER TABLE `users` MODIFY `usuario` VARCHAR(255) NOT NULL;
+-- -- AlterTable
+-- ALTER TABLE `users` MODIFY `usuario` VARCHAR(255) NOT NULL;
 
--- RenameIndex
-ALTER TABLE `users` RENAME INDEX `usuario` TO `user`;
+-- -- RenameIndex
+-- ALTER TABLE `users` RENAME INDEX `usuario` TO `user`;
