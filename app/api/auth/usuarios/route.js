0afe990bff33e178de13b86eff697e89497b5299
AnthonyG21 <126922230+AnthonyG21@users.noreply.github.com>
2024-11-26 21:48:26 -0400

    orden
    orden

--- app/api/auth/usuarios/route.js
+++ app/api/auth/usuarios/route.js
@@ -1,7 +1,110 @@
 import mysql from "mysql2/promise";
 import bcrypt from "bcryptjs";
 
+//Crear (CREATE)
+export async function POST(req){
+  try {
+    const { id, name, lastname, usuario, password, confirmPassword } = await req.json();
 
+    // Validaciones
+    if (!name || !lastname || !usuario || !password) {
+      return new Response(
+        JSON.stringify({ message: "Todos los campos son requeridos" }),
+        { status: 400, headers: { "Content-Type": "application/json" } }
+      );
+    }
+
+    const strongPasswordRegex = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)[A-Za-z\d]{8,}$/;
+    if (!strongPasswordRegex.test(password)) {
+      return new Response(
+        JSON.stringify({
+          message: "La contraseña debe tener al menos 8 caracteres, una mayúscula, una minúscula y un número.",
+        }),
+        { status: 400, headers: { "Content-Type": "application/json" } }
+      );
+    }
+
+    // Crear conexión
+    const connection = await mysql.createConnection({
+      host: process.env.DB_HOST,
+      user: process.env.DB_USERNAME,
+      password: process.env.DB_PASSWORD,
+      database: process.env.DB_DATABASE,
+    });
+
+    // Encriptar la contraseña
+    const hashedPassword = await bcrypt.hash(password, 10);
+    // Crear usuarios
+    await connection.execute(
+      "INSERT INTO users (name, lastname, usuario, password) VALUES (?, ?, ?, ?)",
+      [name, lastname, usuario, hashedPassword]
+    );
+
+    await connection.end();
+
+    return new Response(
+      JSON.stringify({ message: "Usuario creado exitosamente" }),
+      { status: 200, headers: { "Content-Type": "application/json" } }
+    );
+  } catch (error) {
+    console.error("Error al actualizar usuario:", error);
+    return new Response(
+      JSON.stringify({ message: "Error interno del servidor", error: error.message }),
+      { status: 500, headers: { "Content-Type": "application/json" } }
+    );
+  }
+}
+
+//Leer (READ)
+export async function GET(req) {
+  try {
+    const { searchParams } = new URL(req.url);
+    const id = searchParams.get("id"); // Obtener el ID desde los parámetros de la URL
+
+    if (!id) {
+      return new Response(
+        JSON.stringify({ message: "El ID es requerido" }),
+        { status: 400, headers: { "Content-Type": "application/json" } }
+      );
+    }
+
+    // Crear conexión
+    const connection = await mysql.createConnection({
+      host: process.env.DB_HOST,
+      user: process.env.DB_USERNAME,
+      password: process.env.DB_PASSWORD,
+      database: process.env.DB_DATABASE,
+    });
+
+    // Consultar usuario
+    const [user] = await connection.execute(
+      "SELECT * FROM users WHERE id = ?",
+      [id]
+    );
+
+    await connection.end();
+
+    if (user.length === 0) {
+      return new Response(
+        JSON.stringify({ message: "Usuario no encontrado" }),
+        { status: 404, headers: { "Content-Type": "application/json" } }
+      );
+    }
+
+    return new Response(
+      JSON.stringify(user[0]), // Retornar el usuario encontrado
+      { status: 200, headers: { "Content-Type": "application/json" } }
+    );
+  } catch (error) {
+    console.error("Error al obtener usuario:", error);
+    return new Response(
+      JSON.stringify({ message: "Error interno del servidor", error: error.message }),
+      { status: 500, headers: { "Content-Type": "application/json" } }
+    );
+  }
+}
+
+//Actualizar (UPDATE)
 export async function PUT(req) {
   try {
     const { id, name, lastname, password, confirmPassword } = await req.json();
@@ -82,106 +185,7 @@ export async function PUT(req) {
   }
 }
 
-export async function POST(req){
-  try {
-    const { id, name, lastname, usuario, password, confirmPassword } = await req.json();
-
-    // Validaciones
-    if (!name || !lastname || !usuario || !password) {
-      return new Response(
-        JSON.stringify({ message: "Todos los campos son requeridos" }),
-        { status: 400, headers: { "Content-Type": "application/json" } }
-      );
-    }
-
-    const strongPasswordRegex = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)[A-Za-z\d]{8,}$/;
-    if (!strongPasswordRegex.test(password)) {
-      return new Response(
-        JSON.stringify({
-          message: "La contraseña debe tener al menos 8 caracteres, una mayúscula, una minúscula y un número.",
-        }),
-        { status: 400, headers: { "Content-Type": "application/json" } }
-      );
-    }
-
-    // Crear conexión
-    const connection = await mysql.createConnection({
-      host: process.env.DB_HOST,
-      user: process.env.DB_USERNAME,
-      password: process.env.DB_PASSWORD,
-      database: process.env.DB_DATABASE,
-    });
-
-    // Encriptar la contraseña
-    const hashedPassword = await bcrypt.hash(password, 10);
-    // Crear usuarios
-    await connection.execute(
-      "INSERT INTO users (name, lastname, usuario, password) VALUES (?, ?, ?, ?)",
-      [name, lastname, usuario, hashedPassword]
-    );
-
-    await connection.end();
-
-    return new Response(
-      JSON.stringify({ message: "Usuario creado exitosamente" }),
-      { status: 200, headers: { "Content-Type": "application/json" } }
-    );
-  } catch (error) {
-    console.error("Error al actualizar usuario:", error);
-    return new Response(
-      JSON.stringify({ message: "Error interno del servidor", error: error.message }),
-      { status: 500, headers: { "Content-Type": "application/json" } }
-    );
-  }
-}
-
-export async function GET(req) {
-  try {
-    const { searchParams } = new URL(req.url);
-    const id = searchParams.get("id"); // Obtener el ID desde los parámetros de la URL
-
-    if (!id) {
-      return new Response(
-        JSON.stringify({ message: "El ID es requerido" }),
-        { status: 400, headers: { "Content-Type": "application/json" } }
-      );
-    }
-
-    // Crear conexión
-    const connection = await mysql.createConnection({
-      host: process.env.DB_HOST,
-      user: process.env.DB_USERNAME,
-      password: process.env.DB_PASSWORD,
-      database: process.env.DB_DATABASE,
-    });
-
-    // Consultar usuario
-    const [user] = await connection.execute(
-      "SELECT * FROM users WHERE id = ?",
-      [id]
-    );
-
-    await connection.end();
-
-    if (user.length === 0) {
-      return new Response(
-        JSON.stringify({ message: "Usuario no encontrado" }),
-        { status: 404, headers: { "Content-Type": "application/json" } }
-      );
-    }
-
-    return new Response(
-      JSON.stringify(user[0]), // Retornar el usuario encontrado
-      { status: 200, headers: { "Content-Type": "application/json" } }
-    );
-  } catch (error) {
-    console.error("Error al obtener usuario:", error);
-    return new Response(
-      JSON.stringify({ message: "Error interno del servidor", error: error.message }),
-      { status: 500, headers: { "Content-Type": "application/json" } }
-    );
-  }
-}
+//Borrar (DELETE)
 export async function DELETE(req) {
   try {
     const { searchParams } = new URL(req.url);
