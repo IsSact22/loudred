771aaac9ef40ca163cf59534d0d82e44d142eedb
AnthonyG21 <126922230+AnthonyG21@users.noreply.github.com>
2025-02-01 15:35:51 -0400

    revisando avatars

--- app/api/admin/users/[id]/route.js
+++ app/api/admin/users/[id]/route.js
@@ -5,13 +5,12 @@ import path from 'path';
 import bcrypt from "bcryptjs"; // bcryptjs es más compatible con Next.js
 
 function validatePassword(password) {
-  const strongPasswordRegex = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[\W_]).{8,}$/;
-  if (!password || !strongPasswordRegex.test(password)) {
-    return "La contraseña debe tener al menos 8 caracteres, una mayúscula, una minúscula, un número y un carácter especial.";
+  const strongPasswordRegex = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)[A-Za-z\d]{8,}$/;
+    if (!password || !strongPasswordRegex.test(password)) {
+      return new Response(JSON.stringify({ message: "Las contraseñas deben tener mayúsculas, minúsculas, números y un caracter especial" }), { status: 400 });
   }
   return null;
 }
-
 function validateNameField(field, fieldName) {
   const nameRegex = /^[a-zA-Z\s]+$/;
   if (!field || !nameRegex.test(field)) {
@@ -120,134 +119,6 @@ export async function GET(req, { params }) {
 }
 
 //UPDATE
-// export async function PATCH(req, context) {
-//   const { id } = context.params;
-
-//   const { name, lastname, password, confirmPassword, roleId} = await req.json();
-
-//   let connection;
-
-//   try {
-
-//     if (!name && !lastname && !password && !roleId) {
-//       return new Response(
-//         JSON.stringify({ message: "Debe proporcionar al menos un campo para actualizar" }),
-//         { status: 400 }
-//       );
-//     }
-
-//     let updates = [];
-//     let values = [];
-
-//     // Validar y agregar cambios al nombre
-//     if (name) {
-//       const nameError = validateNameField(name, "Nombre");
-//       if (nameError) {
-//         return new Response(JSON.stringify({ message: nameError }), {
-//           status: 400,
-//         });
-//       }
-//       updates.push("name = ?");
-//       values.push(name);
-//     }
-
-//     // Validar y agregar cambios al apellido
-//     if (lastname) {
-//       const lastnameError = validateNameField(lastname, "Apellido");
-//       if (lastnameError) {
-//         return new Response(JSON.stringify({ message: lastnameError }), {
-//           status: 400,
-//         });
-//       }
-//       updates.push("lastname = ?");
-//       values.push(lastname);
-//     }
-
-//     // Validar y agregar cambios a la contraseña
-//     if (password) {
-//       if (password !== confirmPassword) {
-//         return new Response(
-//           JSON.stringify({ message: "Las contraseñas no coinciden" }),
-//           { status: 400 }
-//         );
-//       }
-
-//       const passwordError = validatePassword(password);
-//       if (passwordError) {
-//         return new Response(JSON.stringify({ message: passwordError }), {
-//           status: 400,
-//         });
-//       }
-
-//       const hashedPassword = await bcrypt.hash(password, 10);
-//       updates.push("password = ?");
-//       values.push(hashedPassword);
-//     }
-
-//     // Verificar si el roleId está presente y es válido
-//     if (roleId) {
-//       // Conectar a la base de datos
-//       connection = await mysql.createConnection({
-//         host: process.env.DB_HOST,
-//         user: process.env.DB_USERNAME,
-//         password: process.env.DB_PASSWORD,
-//         database: process.env.DB_DATABASE,
-//       });
-
-//       // Validar que el roleId exista en la base de datos
-//       const [roles] = await connection.execute("SELECT id FROM role WHERE id = ?", [roleId]);
-
-//       if (roles.length === 0) {
-//         return new Response(JSON.stringify({ message: "Role no válido" }), {
-//           status: 400,
-//         });
-//       }
-
-//       updates.push("roleId = ?");
-//       values.push(roleId);
-//     }
-
-//     connection= await mysql.createConnection({
-//       host: process.env.DB_HOST,
-//       user: process.env.DB_USERNAME,
-//       password: process.env.DB_PASSWORD,
-//       database: process.env.DB_DATABASE
-//     })
-
-//     // Asegurar que el usuario existe antes de actualizar
-//     const [existingUser] = await connection.execute("SELECT id FROM User WHERE id = ?", [id]);
-//     if (existingUser.length === 0) {
-//       await connection.end();
-//       return new Response(JSON.stringify({ message: "Usuario no encontrado" }), {
-//         status: 404,
-//       });
-//     }
-
-//     // Ejecutar la actualización
-//     values.push(id); // Agregar el ID al final para WHERE
-//     await connection.execute(
-//       `UPDATE User SET ${updates.join(", ")} WHERE id = ?`,
-//       values
-//     );
-
-//     await connection.end();
-
-//     return new Response(
-//       JSON.stringify({ message: "Usuario actualizado exitosamente" }),
-//       { status: 200 }
-//     );
-//   } catch (error) {
-//     console.error("Error al actualizar usuario:", error);
-//     return new Response("Error interno del servidor", { status: 500 });
-//   } finally {
-//     if (connection) {
-//       await connection.end(); // Asegurarse de cerrar la conexión al final
-//     }
-//   }
-// }
-
-
-//UPDATE
 export async function POST(req, context) {
   const { id } = context.params;
   if (!id) {
@@ -284,31 +155,37 @@ export async function POST(req, context) {
     let updates = [];
     let values = [];
 
+    //cambio de nombre
     if (name) {
       const nameError = validateNameField(name, "Nombre");
       if (nameError) return new Response(JSON.stringify({ message: nameError }), { status: 400 });
       updates.push("name = ?");
       values.push(name);
     }
-
+    //cambio de apellido
     if (lastname) {
       const lastnameError = validateNameField(lastname, "Apellido");
       if (lastnameError) return new Response(JSON.stringify({ message: lastnameError }), { status: 400 });
       updates.push("lastname = ?");
       values.push(lastname);
     }
-
+    //cambio de contraseña
     if (password) {
       if (password !== confirmPassword) {
         return new Response(JSON.stringify({ message: "Las contraseñas no coinciden" }), { status: 400 });
       }
       const passwordError = validatePassword(password);
-      if (passwordError) return new Response(JSON.stringify({ message: passwordError }), { status: 400 });
+      if (passwordError) {
+        return new Response(JSON.stringify({ message: passwordError }), {
+          status: 400,
+        });
+      }
+
       const hashedPassword = await bcrypt.hash(password, 10);
       updates.push("password = ?");
       values.push(hashedPassword);
     }
-
+    //cambio de rol
     if (roleId && !isNaN(roleId)) {
       const [roles] = await connection.execute("SELECT id FROM role WHERE id = ?", [roleId]);
       if (roles.length === 0) {
@@ -317,7 +194,7 @@ export async function POST(req, context) {
       updates.push("roleId = ?");
       values.push(roleId);
     }
-
+    //cambio de avatar
     if (avatar && avatar.name) {
       const allowedExtensions = ["jpg", "jpeg", "png", "webp"];
       const avatarExtension = avatar.name.split(".").pop().toLowerCase();
@@ -328,13 +205,23 @@ export async function POST(req, context) {
       if (!existsSync(uploadsPath)) {
         mkdirSync(uploadsPath, { recursive: true });
       }
+      let avatarPath= null;
       const uniqueFilename = crypto.randomUUID();
       const newAvatarFilename = `${uniqueFilename}.${avatarExtension}`;
       const avatarUploadPath = path.join(uploadsPath, newAvatarFilename);
       const avatarBytes = await avatar.arrayBuffer();
       const avatarBuffer = Buffer.from(avatarBytes);
-      await writeFile(avatarUploadPath, avatarBuffer);
-      const avatarPath = `/avatars/${newAvatarFilename}`;
+      try{
+        await writeFile(avatarUploadPath, avatarBuffer);
+        avatarPath = `/avatars/${newAvatarFilename}`;
+        console.log("Avatar guardado: ", newAvatarFilename)
+      }catch(error){
+        console.error("Error al guardar el avatar: ", error)
+        return new Response(
+          JSON.stringify({ success: false, message: "Error al guardar el avatar" }),
+          { status: 500, headers: { "Content-Type": "application/json" } }
+        );
+      }
       updates.push("avatar = ?");
       values.push(avatarPath);
     }
@@ -360,86 +247,6 @@ export async function POST(req, context) {
   }
 }
 
-// //UPDATE de fotos de perfil
-
-// export async function POST(req, { params }) {
-//   const { id } = params;
-//   if (!id) {
-//     return new Response(JSON.stringify({ message: "ID no proporcionado" }), {
-//       status: 400,
-//     });
-//   }
-//   const formData = await req.formData(); 
-//   const avatar = formData.get("avatar");
-
-//   if (!avatar || !avatar.name) {
-//     return new Response(JSON.stringify({ message: "No se proporcionó una imagen válida" }), {
-//       status: 400,
-//     });
-//   }
-
-//   // Validar extensión del archivo
-//   const allowedExtensions = ["jpg", "jpeg", "png", "webp"];
-//   const avatarExtension = avatar.name.split(".").pop().toLowerCase();
-
-//   if (!allowedExtensions.includes(avatarExtension)) {
-//     return new Response(JSON.stringify({ message: "Formato de imagen no permitido" }), {
-//       status: 400,
-//     });
-//   }
-
-//   const uploadsPath = path.join(process.cwd(), "public/avatars");
-
-//   // Asegurar que la carpeta existe
-//   if (!existsSync(uploadsPath)) {
-//     mkdirSync(uploadsPath, { recursive: true });
-//   }
-
-//   // Crear un nombre único para el archivo
-//   const uniqueFilename = crypto.randomUUID();
-//   const newAvatarFilename = `${uniqueFilename}.${avatarExtension}`;
-//   const avatarUploadPath = path.join(uploadsPath, newAvatarFilename);
-
-//   try {
-//     // Guardar el archivo
-//     const avatarBytes = await avatar.arrayBuffer();
-//     const avatarBuffer = Buffer.from(avatarBytes);
-//     await writeFile(avatarUploadPath, avatarBuffer);
-
-//     // Ruta para la BD
-//     const avatarPath = `/avatars/${newAvatarFilename}`;
-
-//     // Conectar a la BD
-//     const connection = await mysql.createConnection({
-//       host: process.env.DB_HOST,
-//       user: process.env.DB_USERNAME,
-//       password: process.env.DB_PASSWORD,
-//       database: process.env.DB_DATABASE,
-//     });
-
-//     // Verificar si el usuario existe
-//     const [existingUser] = await connection.execute("SELECT id FROM User WHERE id = ?", [id]);
-//     if (existingUser.length === 0) {
-//       await connection.end();
-//       return new Response(JSON.stringify({ message: "Usuario no encontrado" }), {
-//         status: 404,
-//       });
-//     }
-
-//     // Actualizar avatar en la BD
-//     await connection.execute("UPDATE User SET avatar = ? WHERE id = ?", [avatarPath, id]);
-
-//     await connection.end();
-
-//     return new Response(
-//       JSON.stringify({ message: "Foto de perfil actualizada exitosamente", avatarPath }),
-//       { status: 200 }
-//     );
-//   } catch (error) {
-//     console.error("Error al actualizar la foto de perfil:", error);
-//     return new Response("Error interno del servidor", { status: 500 });
-//   }
-// }
 
 
 //DELETE
