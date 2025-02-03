5fb7cf624ee26069986cb1af276dbcccf6fe8761
Isaac Hung <aisaachung@gmail.com>
2025-02-03 15:52:12 -0400

    Validacion Caracteres especiales

--- prisma/users.js
+++ prisma/users.js
@@ -4,28 +4,28 @@ export const User = [
       name: 'Anthony',
       lastname: 'Goncalves',
       username: 'Rollin21',
-      password: 'Hola1234',
+      password: 'Hola1234$',
       role: 'SUPERADMIN', // Este username es un superadmin
     },
     {
       name: 'Liz',
       lastname: 'Ovalles',
       username: 'Rose19',
-      password: 'Taylor123',
+      password: 'Taylor123$',
       role: 'SUPERADMIN', // username común
     },
     {
       name: 'Carlos',
       lastname: 'Gonzalez',
       username: 'Carluno13',
-      password: 'Dembow123',
+      password: 'Dembow123$',
       role: 'SUPERADMIN', // username común
     },
     {
       name: 'Isaac',
       lastname: 'Hung',
       username: 'Chino04',
-      password: 'Chanwis123',
+      password: 'Chanwis123$',
       role: 'SUPERADMIN', // Este username también es un superadmin
     },
   ];
