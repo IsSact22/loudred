0f1f3b43d0da61986a986d4e34dbcbddd1e40abc
Isaac Hung <aisaachung@gmail.com>
2025-02-06 23:29:35 -0400

    SONG ARREGLADO

--- src/partials/songs/SongsForm.jsx
+++ src/partials/songs/SongsForm.jsx
@@ -15,58 +15,58 @@ import { songsSchema } from "@/src/validations/validationSchema";
 // Toast
 import toast from "react-hot-toast";
 
-export default function SongsForm() {
-  const { data: session } = useSession();
-  const role = session?.user?.role?.id;
-  const router = useRouter(); // Hook de Next.js para redirección
+  export default function SongsForm() {
+    const { data: session } = useSession();
+    const role = session?.user?.role?.id;
+    const router = useRouter(); // Hook de Next.js para redirección
 
-  const methods = useForm({
-    resolver: yupResolver(songsSchema),
-    mode: "onChange",
-    defaultValues: {
-      artist: session?.user?.username || "",
-    },
-  });
-  const { handleSubmit } = methods;
-  const { createData, isMutating: isLoading } = useData("/songs", {}, true);
+    const methods = useForm({
+      resolver: yupResolver(songsSchema),
+      mode: "onChange",
+      defaultValues: {
+        artist: session?.user?.username || "",
+      },
+    });
+    const { handleSubmit } = methods;
+    const { createData, isMutating: isLoading } = useData("/songs", {}, true);
 
-  const onSubmit = async (data) => {
-    try {
-      const formData = new FormData();
-      formData.append("title", data.title);
-      formData.append("userId", session?.user?.id);
-      formData.append("categoryId", data.category?.id || "");
-      formData.append("music", data.audio?.[0] || "");
-      formData.append("image", data.cover?.[0] || "");
+    const onSubmit = async (data) => {
+      try { 
+        const formData = new FormData();
+        formData.append("title", data.title);
+        formData.append("userId", session?.user?.id);
+        formData.append("categoryId", data.category?.id || ""); 
+        formData.append("music", data.audio?.[0] || "");
+        formData.append("image", data.cover?.[0] || "");
 
-      // Crear la canción y obtener el id del response
-      const response = await createData(formData);
+        // Crear la canción y obtener el id del response
+        const response = await createData(formData);
 
-      // Si el role es igual a 2, se aprueba automáticamente la canción
-      if (role === 2) {
-        const updateFormData = new FormData();
-        updateFormData.append("validate", "true");
-        await createData(updateFormData, response.id);
-        toast.success("Canción subida con éxito");
+        // Si el role es igual a 2, se aprueba automáticamente la canción
+        if (role === 2) {
+          const updateFormData = new FormData();
+          updateFormData.append("validate", "true");
+          await createData(updateFormData, response.songId);
+          toast.success("Canción subida con éxito");
+        }
+
+        if (role === 1) {
+          toast.success("Su cancion se aprobará en las próximas 24 horas");
+        }
+        
+        router.push("/"); // Redirigir al home
+      } catch (error) {
+        console.error(error);
+        toast.error("Ocurrió un error al subir la canción");
       }
+    };
 
-      if (role === 1) {
-        toast.success("Su cancion se aprobará en las próximas 24 horas");
-      }
-      
-      router.push("/"); // Redirigir al home
-    } catch (error) {
-      console.error(error);
-      toast.error("Ocurrió un error al subir la canción");
-    }
-  };
-
-  const onError = (errors) => {
-    const mensajes = Object.values(errors)
-      .map((error) => error.message)
-      .join(", ");
-    toast.error(`Errores: ${mensajes}`);
-  };
+    const onError = (errors) => {
+      const mensajes = Object.values(errors)
+        .map((error) => error.message)
+        .join(", ");
+      toast.error(`Errores: ${mensajes}`);
+    };
 
   return (
     <div className="relative z-50 flex flex-col items-center justify-center">
